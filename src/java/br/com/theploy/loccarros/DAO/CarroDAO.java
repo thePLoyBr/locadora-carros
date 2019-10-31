c0a4e5af8ce98c105d26fd34b493b2ae8c295010
Felipe <Felipe@DESKTOP-HBGMMPD>
2019-10-31 10:46:07 -0300

    carroDAO busca

--- src/java/br/com/theploy/loccarros/DAO/CarroDAO.java
+++ src/java/br/com/theploy/loccarros/DAO/CarroDAO.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.theploy.loccarros.DAO;
 
 import br.com.theploy.loccarros.entidade.Carro;
@@ -16,10 +11,6 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author felipe
- */
 public class CarroDAO {
 
     public void salvar(Carro carro) {
